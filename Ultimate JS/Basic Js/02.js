67219c5f2b486893907b49234715da5d0360cadf
Ashwin Chauhan <ashwinchauhan99408@gmail.com>
2025-10-01 02:26:11 +0530

    02.js

---
+++ Ultimate JS/Basic Js/02.js
@@ -0,0 +1,24 @@
+//## Declaring Variables
+
+//### JavaScript provides three ways to declare variables:
+
+//#### 1. var (Old way – avoid using in modern code)
+
+
+var name = "Alice";
+
+
+//#### 2. let (Preferred for variables that change)
+
+
+let age = 25;
+age = 26; // Can be updated
+
+
+//#### 3. const (Preferred for constants – can't be changed)
+
+
+const birthYear = 2000;
+// birthYear = 2001; ❌ Error – can't reassign a const
+
+
