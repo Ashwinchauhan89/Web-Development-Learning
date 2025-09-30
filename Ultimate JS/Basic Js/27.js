76b79faaa7ee612098492a5ef2f6ef7718daf116
Ashwin Chauhan <ashwinchauhan99408@gmail.com>
2025-10-01 02:02:23 +0530

    27.js

--- Ultimate JS/Basic Js/27.js
+++ Ultimate JS/Basic Js/27.js
@@ -1,3 +1,6 @@
 // This is example of escape sequence characters in JavaScript
+
 console.log("This is a simple JavaScript file.");   
+
+// Using escape sequences
 console.log("This is a new line.\nThis is the second line.");
