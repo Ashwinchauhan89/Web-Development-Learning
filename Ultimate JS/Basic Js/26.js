7bc497fd4bea101b75e91287ad4b52ca9d08ad80
Ashwin Chauhan <ashwinchauhan99408@gmail.com>
2025-10-01 02:03:55 +0530

    26.js

--- Ultimate JS/Basic Js/26.js
+++ Ultimate JS/Basic Js/26.js
@@ -1,5 +1,9 @@
 // The example of the Template Literal in JavaScript
+
+
 console.log(`This is a example of String`);
 let name = "Ashwin";
 let friend = "Rohan";
-console.log(`My name is  ${name} and his friend name is ${friend}`); //this called the string interpolation
+console.log(`My name is  ${name} and his friend name is ${friend}`); 
+
+//this called the string interpolation
