b2606faf0c27d917d8992e34120111b57b056d1d
Ashwin Chauhan <ashwinchauhan99408@gmail.com>
2025-12-02 23:47:53 +0530

    iwt

---
+++ Ultimate JS/Basic Js/06.js
@@ -0,0 +1,14 @@
+// Arrow function
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(2, 3)); 
+
+
+
+//Method 2
+// Implicit return
+const multiply = (a, b) => a * b;
+
+console.log(multiply(23, 35));
