2c19cec98dd4038690251c74c1a71a15afb8ef5f
Ashwin Chauhan <ashwinchauhan99408@gmail.com>
2026-02-04 14:42:35 +0530

    math

---
+++ Ultimate JS/Basic Js/05.js
@@ -0,0 +1,58 @@
+//Javascripts Number
+let a = 20;
+let b = 3.14;
+
+console.log(b.toFixed(1)); //3.1
+
+// Number
+// let a = 10;
+// let b = 345.6821;
+
+
+// let c = b.toFixed(1);
+// console.log(typeof c);
+
+// console.log(b.toPrecision(4));
+// console.log(b.toString());
+
+
+
+// By using Number function 
+// create a number 
+
+let value = new Number(123); // create but as a object
+console.log(value);
+console.log(typeof value); //object
+
+
+// let a = new Number(20);
+// let b = new Number(20);
+
+// console.log(a==b);
+// / console.log(Boolean(null));
+
+// let b = 20;
+
+// console.log(typeof b);
+
+// let obj1 = {
+//     name:"Rohit"
+// }
+
+// let obj2 = obj1;
+
+// let obj2 = {
+//     name:"Rohit"
+// }
+
+// console.log(obj1==obj2);
+
+
+// Non Primitive: Reference bases pe compare
+// Primitive: Copy by value
+
+// let a = 10;
+// let b = a;
+
+// console.log(a==b);
+
---
+++ Ultimate JS/Basic Js/07.js
@@ -0,0 +1,52 @@
+// Math Fun
+
+ console.log(Math.abs(-4));
+
+console.log(Math.PI);
+console.log(Math.LN10);
+console.log(Math.SQRT2);
+
+
+console.log(Math.ceil(6.3));
+console.log(Math.floor(6.3));
+
+
+console.log(Math.log10(20));
+console.log(Math.max(20,11,3421,12));
+
+
+console.log(Math.random());
+// [0,1): It will generate value between 0 and 1, where 0 is included but 1 is not included
+
+
+// Satebaaji ek game banata:(0-9)
+
+console.log(Math.floor(Math.random()*10)+1); // 1-10
+
+
+
+// Dice Game: 1-6
+
+console.log(Math.floor(Math.random()*6)+1);
+
+// Math.floor(Math.random()*totalNumberOfOutcome)+shift
+
+
+// 15-25
+
+
+console.log(Math.floor(Math.random()*11)+15);
+
+
+
+// Formula for any range (min -max):
+
+// Math.floor(Math.random()*(max-min+1))+min
+
+
+
+
+// OTP Generate: 4 digit: 1000-9999
+
+
+// console.log(Math.floor(Math.random()*(9999-1000+1))+1000);
