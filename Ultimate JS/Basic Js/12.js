60228d1abce5398a33f33d7e2314eaba38b62039
Ashwin Chauhan <ashwinchauhan99408@gmail.com>
2026-02-06 22:25:29 +0530

    Commit 2 of the day

---
+++ Ultimate JS/Basic Js/12.js
@@ -0,0 +1,34 @@
+const nowa = new Date();
+
+console.log(nowa);
+// console.log(now.toString());
+// console.log(now.toISOString());
+// console.log(now.toLocaleString());
+
+
+// Local time chal rha
+// console.log(now.getDay());
+// console.log(now.getDate());
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getHours());
+// console.log(now.getSeconds());
+
+
+// days: Mon-tue(1 based)
+// Month: 0 1
+
+// year month date hour minute second millisecond
+// const now = new Date(2025,8,20,8,25,16,125);
+
+// console.log(now.toString());
+
+
+// Timestamp
+const now = Date.now();
+const dates = new Date(1759275037293);
+// console.log(dates.toString());
+
+console.log(dates);
+console.log(now);
+// 1759272851288: TimeStamp
